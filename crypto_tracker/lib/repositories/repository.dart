88d615590b31d571f1ab5e11fc92295cd396fbaf
João Nascimento <joaovc.silva2003@gmail.com>
2024-06-06 20:55:09 -0300

    Update repository.dart

--- crypto_tracker/lib/repositories/repository.dart
+++ crypto_tracker/lib/repositories/repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:crypto_tracker/models/coin_enum.dart';
+import 'package:crypto_tracker/models/preco.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,13 +11,16 @@ class Repository {
   //Nos "headers" da solicitação tem q colocar:
   //"X-CoinAPI-Key": "YOUR-API-KEY"
 
-  static Future<Map<String,dynamic>> getExchange(CoinEnum deMoeda, CoinEnum paraMoeda) async {
+  static Future<Preco> getExchange(CoinEnum deMoeda, CoinEnum paraMoeda) async {
     await dotenv.load(fileName: "keys.env");
     String apiKey = dotenv.env["API_KEY"]!;
-    var url = Uri.https("rest.coinapi.io", "/v1/exchangerate/${deMoeda.name}/${paraMoeda.name}");
-    http.Response response = await http.get(url, headers: {"X-CoinAPI-Key": apiKey});
-    Map<String,dynamic> retorno = json.decode(response.body);
-    print(retorno);
-    return retorno;
+    var url = Uri.https("rest.coinapi.io",
+        "/v1/exchangerate/${deMoeda.name}/${paraMoeda.name}");
+    http.Response response =
+        await http.get(url, headers: {"X-CoinAPI-Key": apiKey});
+    Map<String, dynamic> retorno = json.decode(response.body);
+    Preco preco =
+        Preco(deMoeda: deMoeda, paraMoeda: paraMoeda, valor: retorno["rate"]);
+    return preco;
   }
 }
